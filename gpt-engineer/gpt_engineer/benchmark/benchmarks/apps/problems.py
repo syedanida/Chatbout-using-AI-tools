d4d2f3b5a1948946af60665c90a75ede3d10474f
Syeda Nida Khader <60160145+syedanida@users.noreply.github.com>
2024-10-02 14:53:10 -0700

    Delete gpt-engineer directory

--- gpt-engineer/gpt_engineer/benchmark/benchmarks/apps/problems.py
+++
@@ -1,3 +0,0 @@
-# TODO: Pick problems
-# Temporary testing against these problems
-PROBLEM_IDS = list(range(0, 50))
--- gpt-engineer/gpt_engineer/benchmark/benchmarks/mbpp/problems.py
+++
@@ -1,3 +0,0 @@
-# TODO: Pick problems
-# Temporary testing against these problems
-PROBLEM_IDS = range(0, 100)
